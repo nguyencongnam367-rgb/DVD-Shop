560daa1ce512fdced298d4abb10369c91ad2b4ea
NDTA <anh35830@gmail.com>
2026-09-15 22:06:44 +0700

    đã đồng bộ

--- src/resources/Routers/index.js
+++ src/resources/Routers/index.js
@@ -1,15 +1,15 @@
-const home = require('./homeRoutes');
-const auth = require('./authRoutes');
-const cart = require('./cartRoutes');
-const product = require('./productRoutes');
-const quantri = require('./quantri');
+const home = require("./homeRoutes");
+const auth = require("./authRoutes");
+const cart = require("./cartRoutes");
+const product = require("./productRoutes");
+const quantri = require("./quantri");
 
 function Routes(app) {
-    app.use('/', home);
-    app.use('/auth', auth);
-    app.use('/cart', cart);
-    app.use('/product', product);
-    app.use('/products', product);
-    app.use('/', quantri);
+  app.use("/", home);
+  app.use("/auth", auth);
+  app.use("/cart", cart);
+  app.use("/product", product);
+  app.use("/products", product);
+  app.use("/admin", quantri);
 }
 module.exports = Routes;
